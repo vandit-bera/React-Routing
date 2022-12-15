e1ccd013c5e7e783efbe930086cf9dcc61d3740a
vandit <vanditbera@gmail.com>
2022-12-15 19:55:46 +0530

    Add app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import {Routes, Route} from "react-router-dom";
 import About from "./component/About";
 import Blog from "./component/Blog";
+import Contact from "./component/Contact";
 import Home from "./component/Home";
 import Navbar from "./component/Navbar";
+import Services from "./component/Services";
 import Team from "./component/Team";
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
       <Route path="/blog" element={ <Blog /> } />
       <Route path="/team" element={ <Team /> } />
       <Route path="/about" element={ <About /> } />
+      <Route path="/services" element={ <Services /> } />
+      <Route path="/contact" element={ <Contact /> } />
     </Routes>
     </>
   );
